580cc0784949c893633893ee8ef8d42120cc3cff
Heischichou <jangarot@gmail.com>
2024-04-13 13:11:28 +0800

    Create 1:M queries between Employee and Company types

--- schema/schema.js
+++ schema/schema.js
@@ -9,14 +9,59 @@ const {
   GraphQLList,
   GraphQLNonNull,
 } = graphql;
-const { v4 } = require('uuid');
+const axios = require("axios");
+const { v4 } = require("uuid");
 
 const uuid = new GraphQLObjectType({
   name: "uuid",
   fields: () => ({
     v4: {
       type: new GraphQLNonNull(GraphQLString),
-      resolve: () => v4(),
+      resolve: () => `Query ID ${v4()}`,
+    },
+  }),
+});
+
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    geo: { type: new GraphQLNonNull(GraphQLString) },
+    employees: {
+      type: new GraphQLNonNull(new GraphQLList(EmployeeType)),
+      resolve: async (root, args) =>
+        await axios
+          .get(`http://localhost:3000/employees?companyId=${root.id}`)
+          .then((res) => res.data),
+    },
+  }),
+});
+
+const EmployeeType = new GraphQLObjectType({
+  name: "Employee",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    position: { type: new GraphQLNonNull(GraphQLString) },
+    gender: {
+      type: new GraphQLEnumType({
+        name: "Gender",
+        values: {
+          Male: { value: "Male" },
+          Female: { value: "Female" },
+          Other: { value: "Other" },
+        },
+      }),
+    },
+    age: { type: new GraphQLNonNull(GraphQLInt) },
+    active: { type: new GraphQLNonNull(GraphQLBoolean) },
+    company: {
+      type: new GraphQLNonNull(CompanyType),
+      resolve: async (root, args) =>
+        await axios
+          .get(`http://localhost:3000/companies/${root.companyId}`)
+          .then((res) => res.data),
     },
   }),
 });
@@ -27,6 +72,20 @@ const RootQuery = new GraphQLObjectType({
     uuid: {
       type: uuid,
       resolve: () => ({}),
+    },
+    getCompanies: {
+      type: new GraphQLList(CompanyType),
+      resolve: async (root) =>
+        await axios
+          .get(`http://localhost:3000/companies`)
+          .then((res) => res.data),
+    },
+    getEmployees: {
+      type: new GraphQLList(EmployeeType),
+      resolve: async (root) =>
+        await axios
+          .get(`http://localhost:3000/employees`)
+          .then((res) => res.data),
     },
   },
 });
